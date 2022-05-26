a3b93948d4da378b0e4885d09594c39c72434727
lilach <lilachgro@gmail.com>
2022-05-26 15:04:28 +0300

    handle buttons pressed.

--- SmartSitterClient/app/src/main/java/com/example/smartsitterclient/ReservationLayoutLab.kt
+++ SmartSitterClient/app/src/main/java/com/example/smartsitterclient/ReservationLayoutLab.kt
@@ -2,24 +2,26 @@ package com.example.smartsitterclient
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
-class ReservationLayoutLab : AppCompatActivity() {
-    private var buttonClick22: Button? = null
+class ReservationLayoutLab : AppCompatActivity(), View.OnClickListener {
+    private var buttonClickSendReservation: Button? = null
     private var viewPreviousData: TextView? = null
     private var viewChosenChairId: TextView? = null
-
-    private var chairIdButton1: Button? = null
+    private var setChairs = setOf<Button>()
+    private var chairIdNumber = 0
 
     private fun functionPerChair(chairId: String, localViewChosenChairId: TextView) {
-        Toast.makeText(this, "click1", Toast.LENGTH_SHORT).show()
         val temp = "chosen chair: $chairId"
         localViewChosenChairId.text = temp
     }
+
+    private fun enableChairs(){}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,33 +43,49 @@ class ReservationLayoutLab : AppCompatActivity() {
             viewPreviousData = localPreviousData
         }
 
-        val localViewChosenChairId = viewChosenChairId
-
         //define all the chairs:
-        chairIdButton1 = findViewById(R.id.chair_id_1)
-        val localChairIdButton1 = chairIdButton1
-        localChairIdButton1?.setOnClickListener {
-            if (localViewChosenChairId != null) {
-                functionPerChair("1", localViewChosenChairId)
-                chairIdButton1?.isSelected = true
-            }
-        }
-        chairIdButton1 = localChairIdButton1
-        //finish to define all the chairs
+        val chairIdButton1 :Button = findViewById(R.id.chair_id_1)
+        val chairIdButton2 :Button = findViewById(R.id.chair_id_2)
 
-        viewChosenChairId = localViewChosenChairId
+        setChairs = setChairs.plusElement(chairIdButton1)
+        setChairs = setChairs.plusElement(chairIdButton2)
+
+        chairIdButton1.setOnClickListener(this)
+        chairIdButton2.setOnClickListener(this)
+        //finish to define all the chairs
 
         val isEnabled = false
         if (isEnabled){
-            chairIdButton1?.isEnabled = false
+            chairIdButton1.isEnabled = false
         }
 
-        buttonClick22 = findViewById(R.id.send_location)
-        val localButtonClick22 = buttonClick22
-        localButtonClick22?.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+        buttonClickSendReservation = findViewById(R.id.send_location)
+        val localButtonClickSendReservation = buttonClickSendReservation
+        localButtonClickSendReservation?.setOnClickListener {
+            if (chairIdNumber != 0){
+                // some chair is chosen and chairIdNumber contain it.
+                // connect to server and try to insert the values, and return the result.
+                startActivity(Intent(this, MainActivity::class.java))
+            }
             //finish() //with this command the previous page closed and can't be reach with the back button (that built in the phone)
         }
-        buttonClick22 = localButtonClick22
+        buttonClickSendReservation = localButtonClickSendReservation
+    }
+
+    override fun onClick(v: View?) {
+        val localViewChosenChairId = viewChosenChairId
+        val idChair = v?.id
+        viewChosenChairId = localViewChosenChairId
+        for (b in setChairs) {
+            b.isSelected = false
+            if (idChair != null && idChair == b.id){
+                b.isSelected = true
+                if (localViewChosenChairId != null) {
+                    val buttonText = b.text.toString()
+                    chairIdNumber = buttonText.toInt()
+                    functionPerChair(buttonText, localViewChosenChairId)
+                }
+            }
+        }
     }
 }
